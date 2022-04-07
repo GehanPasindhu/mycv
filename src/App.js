9ec255c3e81a0940651591bbd2bbddfc563269b6
GehanPasindhu <pasindhu97@gmail.com>
2022-04-07 06:22:24 +0530

    googleTags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from 'react-ga';
 import GlobalStyle from "./globalStyle";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
@@ -7,6 +8,14 @@ import Footer from "./components/Footer/Footer";
 import Recomendations from "./components/Recomendations/Recomendations";
 
 function App() {
+
+  const TRACKING_ID = "G-38V66T52X1";
+  ReactGA.initialize(TRACKING_ID);
+
+useEffect(() => {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+},[]);
+
   return (
  <Router>
    <GlobalStyle />
